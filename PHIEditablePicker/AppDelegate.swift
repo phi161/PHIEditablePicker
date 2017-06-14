d532dd503eadfba4bddc073ea7b7f9df66d809ce
phi161 <phi161@users.noreply.github.com>
2017-06-14 10:16:43 +0300

    Initial project files

---
+++ PHIEditablePicker/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  PHIEditablePicker
+//
+//  Created by phi161 on 2017.06.14.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+}
---
+++ PHIEditablePicker/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  PHIEditablePicker
+//
+//  Created by phi161 on 2017.06.14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
