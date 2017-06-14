544a102c793fe6e51aadbb5fbe3c893b745573c7
phi161 <phi161@users.noreply.github.com>
2017-06-14 10:27:45 +0300

    Use the component

--- PHIEditablePicker/ViewController.swift
+++ PHIEditablePicker/ViewController.swift
@@ -9,5 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    //MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        statusLabel.text = ""
+    }
+    
+    //MARK: - Actions
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        let picker = PHIPickerViewController(options: ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight"], editableOptions: ["00", "11", "22", "33", "44", "55", "66", "77", "88", "99"])
+        picker.delegate = self
+        let navigationController = UINavigationController(rootViewController: picker)
+        present(navigationController, animated: true, completion: nil)
+    }
 }
 
+
+//MARK: - PHIPickerDelegate
+
+extension ViewController: PHIPickerDelegate {
+    func pickerViewControllerDidCancel(controller: PHIPickerViewController) {
+        controller.dismiss(animated: true, completion: nil)
+        statusLabel.text = "user canceled"
+    }
+    
+    func pickerViewController(controller: PHIPickerViewController, didSelectOption option: IndexPath?, _ text: String?) {
+        controller.dismiss(animated: true) {
+            self.statusLabel.text = "done with option \(option?.row ?? -1) and text \(text ?? "nil")"
+        }
+    }
+}
